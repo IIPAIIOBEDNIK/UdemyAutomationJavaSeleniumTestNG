eb122ba2bc107f2d4edefa403ac716d17952dd80
3yd <pereponov_alexander@mail.ru>
2023-09-24 00:48:44 +0300

    Added description about naming variables

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class Start {
     public static void main(String[] args) {
 
---
+++ src/main/java/org/example/Variables.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int num = 141;
+        // Допускается начинать переменную с символов "_" и "$"
+        // В переменной могут содержаться знаки "_", "$", буквы и цифры
+        // Рекомендуется использовать нотацию CamelCase - numberBuildingStreet
+
+    }
+}
