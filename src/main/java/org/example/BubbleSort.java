c1eeff20f30e6811b4acc4b92356ee352c9a6c4e
3yd <pereponov_alexander@mail.ru>
2023-11-09 23:45:43 +0300

    Added BubbleSort

---
+++ src/main/java/org/example/BubbleSort.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] nums = {2, 1, 3, 6, 9, 4, 153, 0};
+        int temp;
+
+        boolean sorted = false;
+
+        while(!sorted) {
+            sorted = true;
+
+            for (int i = 0; i < nums.length - 1; i++){
+                if (nums[i] > nums[i+1]){
+                    temp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+        for (int i : nums){
+            System.out.println(i);
+        }
+    }
+
+}
